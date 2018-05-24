0229a2078b6d7b351f96683f50bd420f57fae695
Brandon Green <brandon@brandonleeproductions.com>
2018-05-24 02:38:29 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
-  if array.count < 3
-    array.join(" and ")
+  if array.count < 3  #If the array has less than 3 indexes
+    array.join(" and ") #Put " and " in between the 2 indexes
   else
     array[0, array.length - 1].join(", ") + ", and " + array.last
   end
